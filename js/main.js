22f7301489ee0be95e660ed61d7352beebd1fa8e
stevlabs <steven.arce.arebalo@gmail.com>
2024-09-29 12:53:14 +0200

    Comentado el ejercicio 3

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/*
+EJERCICIO 3 
+Crea un array de objetos que representen libros, cada uno con las propiedades titulo, autor, y anio. Escribe una función que reciba el array y devuelva solo los libros publicados después del año 2000.
+*/
+
+/*
+console.log("---------- Ejercicio 1 ----------");
+
+const arrObjetos = [
+    {
+        titulo: "Título de libro 1",
+        autor: "Autor 1",
+        anio: 1990
+    },
+    {
+        titulo: "Título de libro 2",
+        autor: "Autor 2",
+        anio: 2005
+    },
+    {
+        titulo: "Título de libro 3",
+        autor: "Autor 3",
+        anio: 2010
+    }
+];
+
+const filtrarLibros = () => {
+    arrObjetos.forEach(item => {
+        if (item.anio >= 2000) {
+            console.log(item)
+        }
+    });
+}
+
+filtrarLibros();
+
+console.log("---------------------------------");
+*/
