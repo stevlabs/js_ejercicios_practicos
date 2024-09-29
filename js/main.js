e3cade506b590f5305d759ea1b4dddaac8a3d67f
stevlabs <steven.arce.arebalo@gmail.com>
2024-09-29 13:50:09 +0200

    Ejercicio 1 resuelto: añadido pseudocdigo, creada la funcion sum y resultado mostrado en HTML.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
 /*
+EJERCICIO 1 - Escribe una función que reciba un array de números y devuelva la suma de todos los elementos.
+
+PSEUDOCODIGO:
+
+INICIO
+    resultado = 0
+    // iterar cada elemento del array
+    PARA CADA num EN array HACER
+        // sumar el elemento actual al resultado
+        resultado = resultado + num
+    FIN PARA
+    ESCRIBIR "La suma de array es: resultado"
+FIN
+*/
+
+const sum = (array)=>{
+    const resultado = array.reduce((acumulador, num) => {
+        return acumulador + num;
+    }, 0);
+
+    document.getElementById("ejercicio1").innerHTML =
+    `La suma del array ${array} es: ${resultado}`
+};
+
+sum([5,10,3,2]);
+
+/*
 EJERCICIO 3 
 Crea un array de objetos que representen libros, cada uno con las propiedades titulo, autor, y anio. Escribe una función que reciba el array y devuelva solo los libros publicados después del año 2000.
 */
